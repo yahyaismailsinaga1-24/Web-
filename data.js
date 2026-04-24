f57058e89d154dcd1e732d7ed898352cd9b591ee
yahyaa <yahyaismailsinaga0@gmail.com>
2026-04-24 11:03:58 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 const databaseProyek = {
     "1": {
-        judul: "Sistem POS untuk PT EPEP BERSAUDARA",
-        keahlian: ["Javasricpt", "PHP" ],
-        gambar: "https://i.pinimg.com/736x/3d/f4/1a/3df41a88de33e733bae2f22cf4695332.jpg",
-        latar: "Ini adalah proyek saya yanng saya kerjakan selama masa pelatihan",
-        tantangan: "kebutuhan dari PT cukup susah",
-        pelajaran: "Saya bisa mengembangkan skil programing",
+        judul: "Sistem POS untuk PT. EPEP BERSAUDARA",
+        keahlian: ["Javasricpt", "PHP" , "MySQL", "HTML", "CSS" ],
+        gambar: "https://i.pinimg.com/736x/d5/07/cd/d507cd4401e293cb44ec36477ea758ab.jpg",
+        latar: "Ini adalah proyek saya yanng saya kerjakan selama masa pelatihan di PT. EPEP BERSAUDARA, dimana saya ditugaskan untuk membuat sebuah sistem POS untuk membantu proses penjualan di toko mereka",
+        tantangan: "kebutuhan dari PT cukup susah untu dipenuhi karena saya masih pemula",
+        pelajaran: "Saya bisa mengembangkan skil programing saya dengan membuat sistem POS ini",
         link: ""
     },
 };
