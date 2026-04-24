2286684c2b95f85057ab30ada074a30cfefdad52
yahyaa <yahyaismailsinaga0@gmail.com>
2026-04-24 09:22:30 +0700

    upload github pertamaku sebagai programer

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "1": {
+        judul: "Sistem POS untuk PT EPEP BERSAUDARA",
+        keahlian: ["Javasricpt", "PHP" ],
+        gambar: "https://i.pinimg.com/736x/3d/f4/1a/3df41a88de33e733bae2f22cf4695332.jpg",
+        latar: "Ini adalah proyek saya yanng saya kerjakan selama masa pelatihan",
+        tantangan: "kebutuhan dari PT cukup susah",
+        pelajaran: "Saya bisa mengembangkan skil programing",
+        link: ""
+    },
+};
